e3ec8c4f5eaeafc8667e210ff7ec842e233ef506
a-trusov <trusov@haulmont.com>
2021-08-03 18:06:50 +0400

    Пример с AnnotationConfigApplicationContext

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,13 +1,16 @@
 package lesson1;
 
+import lesson1.configs.MainConfig;
 import lesson1.services.SimpleService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/context.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/context.xml");
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
         SimpleService bean = context.getBean(SimpleService.class);
         System.out.println(bean.getTestService());
     }
